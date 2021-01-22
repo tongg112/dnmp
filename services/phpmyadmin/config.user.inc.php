4080062c91b6d41a386ab1810c026605b99ade19
xtong <lotongok@gmail.com>
2021-01-22 21:37:00 +0800

    phpmyadmin登录两个版本的mysql设置

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -16,4 +16,15 @@ $i++; // server 2 :
 $cfg['Servers'][$i]['verbose']   = 'no2';
 $cfg['Servers'][$i]['host']      = 'mysql5';
 */
+$cfg['blowfish_secret']='multiServerExample70518';
 
+$i = 0;
+
+$i++; // server 1 :
+$cfg['Servers'][$i]['verbose']   = '8';
+$cfg['Servers'][$i]['host']      = 'mysql';
+
+
+$i++; // server 2 :
+$cfg['Servers'][$i]['verbose']   = '5';
+$cfg['Servers'][$i]['host']      = 'mysql5';
